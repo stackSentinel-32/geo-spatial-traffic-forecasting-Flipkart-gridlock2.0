4727df2f2683c368ded3c6a104d9bb82afa3ad93
Piyush Madheshiya <piyushsmmadheshiya08@gmail.com>
2026-06-11 00:30:07 +0530

    updated the model's pipeline

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,15 +1,4 @@
-import config
 from pipeline import run_pipeline
 
-
-def main():
-    run_pipeline(
-        train_path=config.TRAIN_PATH,
-        test_path=config.TEST_PATH,
-        submit_dir=config.SUBMIT_DIR,
-        v2_submission_path=config.V2_SUBMISSION_PATH,
-    )
-
-
 if __name__ == '__main__':
-    main()
+    run_pipeline()
